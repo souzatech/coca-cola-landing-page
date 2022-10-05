4ad41b7c8d623aab8a0f8d4bf06eb8d9ea3d0cd6
Thiago <thiagosouzatech@gmail.com>
2022-10-05 05:33:26 -0300

    coke-turn-animation-added

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ window.addEventListener("scroll", function() {
   let header = document.querySelector("#home");
   header.classList.toggle("sticky", window.scrollY > 0)
 })
+
+window.addEventListener("scroll", activateCokeTurnAnimation)
+
+function activateCokeTurnAnimation() {
+  let cokeImg = document.querySelector("#slider-image")
+  console.log(window.scrollY);
+  cokeImg.classList.toggle("active", window.scrollY >= 400 && window.scrollY <= 1000)
+}
